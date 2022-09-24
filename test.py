cf9aa9bf5636cbf0562c5df0ad34240143b6eba6
tednguyendev <76901736+tednguyendev@users.noreply.github.com>
2022-09-24 17:40:03 +0700

    add customs

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import base64
 import banana_dev as banana
 
 #Needs test.mp3 file in directory
-with open(f'test.mp3','rb') as file:
+with open(f'test.m4a','rb') as file:
     mp3bytes = BytesIO(file.read())
 mp3 = base64.b64encode(mp3bytes.getvalue()).decode("ISO-8859-1")
 model_payload = {"mp3BytesString":mp3}
